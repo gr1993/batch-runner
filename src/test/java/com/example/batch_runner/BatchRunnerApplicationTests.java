5f83c1ea0618271f9d9a0f9d89e0b7d9079210da
hppro2024\smcore2024 <klpark@smcore.co.kr>
2025-05-13 13:51:56 +0900

    feat: batch-runner 프로젝트 생성

---
+++ src/test/java/com/example/batch_runner/BatchRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batch_runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
