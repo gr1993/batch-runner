35a3b2b7a7c23971ed70a2fe9be2f3df356e5c64
hppro2024\smcore2024 <klpark@smcore.co.kr>
2025-06-10 15:00:52 +0900

    fix(core) : routeStopExcelParallelJob에서 Partitioning 방식 병렬 처리 로직 추가

--- batch-core/src/main/java/com/example/batch_runner/job/batch/RouteStopExcelParallelJobConfig.java
+++ batch-core/src/main/java/com/example/batch_runner/job/batch/RouteStopExcelParallelJobConfig.java
@@ -6,22 +6,30 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.extensions.excel.RowMapper;
 import org.springframework.batch.extensions.excel.poi.PoiItemReader;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @Slf4j
@@ -34,29 +42,78 @@ public class RouteStopExcelParallelJobConfig {
 
     private static final int CHUNK_SIZE = 5000;
 
+    // 엑셀 파일을 물리적으로 나눔
+    private final List<String> fileNames = List.of(
+            "nodeData_part1.xlsx",
+            "nodeData_part2.xlsx",
+            "nodeData_part3.xlsx",
+            "nodeData_part4.xlsx",
+            "nodeData_part5.xlsx"
+    );
 
     @Bean
     public Job routeStopExcelParallelJob(@Qualifier("deleteMissingRouteStopsStep") Step deleteMissingRouteStopsStep, Set<RouteStopInfoId> validRouteStopKeys) {
         return new JobBuilder("routeStopExcelParallelJob", jobRepository)
-                .start(routeStopExcelParallelStep(validRouteStopKeys))
+                .start(excelParallelMasterStep(validRouteStopKeys))
                 .next(deleteMissingRouteStopsStep) // 오래 안걸리는 Step은 재사용
                 .build();
     }
 
     @Bean
-    public Step routeStopExcelParallelStep(Set<RouteStopInfoId> validRouteStopKeys) {
+    public Step excelParallelMasterStep(Set<RouteStopInfoId> validRouteStopKeys) {
         return new StepBuilder("routeStopExcelParallelStep", jobRepository)
+                .partitioner("excelParallelSlaveStep", excelFilePartitioner())
+                .step(excelParallelSlaveStep(validRouteStopKeys))
+                .gridSize(5)
+                .taskExecutor(taskExecutor())
+                .build();
+    }
+
+    @Bean
+    public Step excelParallelSlaveStep(Set<RouteStopInfoId> validRouteStopKeys) {
+        return new StepBuilder("excelParallelSlaveStep", jobRepository)
                 .<RouteStopInfo, RouteStopInfo>chunk(CHUNK_SIZE, transactionManager)
-                .reader(excelParallelItemReader())
+                .reader(excelPartitionedReader(null))
                 .processor(excelParallelItemProcessor(validRouteStopKeys))
                 .writer(excelParallelItemWriter())
                 .build();
     }
 
     @Bean
-    public ItemReader<RouteStopInfo> excelParallelItemReader() {
+    public Partitioner excelFilePartitioner() {
+        return gridSize -> {
+            Map<String, ExecutionContext> partitions = new HashMap<>();
+            for (int i = 0; i < fileNames.size(); i++) {
+                ExecutionContext context = new ExecutionContext();
+                context.putString("fileName", fileNames.get(i));
+                partitions.put("partition" + i, context);
+            }
+            return partitions;
+        };
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        int coreSize = Runtime.getRuntime().availableProcessors();
+        int poolSize = Math.min(fileNames.size(), coreSize); // 파일 갯수가 코어수보다 많으면 코어수만큼 설정
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(poolSize);
+        taskExecutor.setMaxPoolSize(poolSize);
+        taskExecutor.setQueueCapacity(25);
+        taskExecutor.setThreadNamePrefix("batch-executor-");
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+    /**
+     * 이 빈 반환 메서드는 ItemReader가 아닌 PoiItemReader로 반환해야 한다.
+     * 병렬 처리 시 ItemReader로 반환하면 doOpen을 실행하지 않음
+     */
+    @Bean
+    @StepScope
+    public PoiItemReader<RouteStopInfo> excelPartitionedReader(@Value("#{stepExecutionContext['fileName']}") String fileName) {
         PoiItemReader<RouteStopInfo> reader = new PoiItemReader<>();
-        reader.setResource(new ClassPathResource("/data/nodeData.xlsx"));
+        reader.setResource(new ClassPathResource("/data/" + fileName));
         reader.setRowMapper(excelRowMapper());
         reader.setLinesToSkip(1);
         return reader;
